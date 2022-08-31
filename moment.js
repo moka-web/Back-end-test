29aae50fcb7ed030c55222e1122c2b2afe90320e
tomas juarez <mokajua@gmail.com>
2022-08-31 19:25:33 -0300

    clean

--- moment.js
+++
@@ -1,9 +0,0 @@
-const moment = require("moment");
-
-let day = moment()
-let bornDay = moment("19930811","YYYYMMDD")
-let years= day.diff(bornDay,"years")
-let days = bornDay.diff(day,"days")
-console.log(days)
-
-
